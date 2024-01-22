3ab6b293ba6ae11c73daf5a0d9abfff59677e168
Cesarq19 <cepaquin@espol.edu.ec>
2024-01-21 22:03:37 -0500

    Cambio en el mapa

--- s_nav2/launch/navigation2.launch.py
+++ s_nav2/launch/navigation2.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
         default=os.path.join(
             get_package_share_directory('s_nav2'),
             'map',
-            'map.yaml'))
+            'ramel.yaml'))
 
     param_dir = LaunchConfiguration(
         'params_file',
